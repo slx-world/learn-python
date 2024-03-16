73ac8d2dfa216f78d1d8201a9cd045c40ddb8b11
slx <slx9920@qq.com>
2024-03-16 21:43:00 +0800

    注释

---
+++ chapter01/test001.py
@@ -0,0 +1,25 @@
+"""
+@Author: Song Laixiong
+@Create: 2024-03-16
+@Describe: 注释
+"""
+
+# 单行注释
+
+""""
+    多行注释
+    第一行注释
+    第二行注释
+    第三行注释
+"""
+
+''''
+    多行注释
+    第一行注释
+    第二行注释
+    第三行注释
+'''
+
+# 输出hello world
+print('hello world')
+print('hello Python')   # 输出hello Python
