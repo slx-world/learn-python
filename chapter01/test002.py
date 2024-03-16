107f45fb47f3715541f52b11baa0bafde3a3d00d
slx <slx9920@qq.com>
2024-03-16 21:55:55 +0800

    变量

---
+++ chapter01/test002.py
@@ -0,0 +1,35 @@
+"""
+@Author: Song Laixiong
+@Create: 2024-03-16
+@Describe: 变量
+"""
+
+my_name = 'TOM'
+print(my_name)
+
+schoolName = '黑马程序员'
+print(schoolName)
+
+a = 1
+print(type(a))  # 整型
+
+b = 1.1
+print(type(b))  # 浮点型
+
+c = True
+print(type(c))  # 布尔型
+
+d = '12345'
+print(type(d))  # 字符串
+
+e = [10, 20, 30]
+print(type(e))  # 列表
+
+f = (10, 20, 30)
+print(type(f))  # 元组
+
+h = {10, 20, 30}
+print(type(h))  # 集合
+
+g = {'name': 'TOM', 'age': 20}
+print(type(g))   # 字典
